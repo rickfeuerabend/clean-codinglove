25a1187f24ce62f060592b9d471e0fbe250322b0
Rick Feuerabend <feuerabend@ironshark.de>
2018-12-17 14:55:57 +0100

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// jshint ignore: start
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import * as qs from 'query-string';
@@ -10,7 +11,17 @@ class App extends Component {
     this.state = {};
   }
 
+  // getRandomUrl() {
+  // console.log('Load thecodinglove.com');
+
+  // fetch(
+  //   'https://cors-anywhere.herokuapp.com/https://thecodinglove.com/',
+  // ).then(response => console.log(response));
+  // }
+
   render() {
+    // this.getRandomUrl();
+
     // eslint-disable-next-line
     const queryParams = qs.parse(location.search);
     return <TheCodingLove gif={queryParams.gif} title={queryParams.title} />;
